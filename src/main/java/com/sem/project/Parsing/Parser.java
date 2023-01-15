dd9d26d504e97ac4db31762a70a3b66cea6f1b2b
Sashmin <tolochko.minsk@gmail.com>
2023-01-15 16:13:08 +0300

    Parser refactoring

--- src/main/java/com/sem/project/Parsing/Parser.java
+++ src/main/java/com/sem/project/Parsing/Parser.java
@@ -8,45 +8,13 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-    static String parseExpressions(String str) {
-        Pattern mainRegex = Pattern.compile(
-                "(cos|sin|tan|log|log2|abs)?\\(-?\\d+\\.?\\d*([+\\-/*^%]-?\\d+\\.?\\d*)*\\)"
-        );
-        Matcher matcher;
+    public static int validMatchesAmount = 1;
 
-        int validMatchesCount = 1;
-        int searchStartIndex;
 
-        while (validMatchesCount != 0) {
-            validMatchesCount = 0;
-            searchStartIndex = 0;
-            matcher = mainRegex.matcher(str);
-
-            while (matcher.find(searchStartIndex)) {
-                double result = Double.MAX_VALUE;
-                try {
-                    Expression exp = new ExpressionBuilder(matcher.group()).build();
-                    result = exp.evaluate();
-                }
-                catch (Exception e) {}
-                searchStartIndex = matcher.start();
-                if (result != Double.MAX_VALUE) {
-                    CharSequence strResult = String.valueOf(result);
-                    CharSequence curMatch = matcher.group();
-                    str = str.replace(matcher.group(), strResult);
-                    searchStartIndex += strResult.length();
-                    validMatchesCount++;
-                }
-                else {
-                    searchStartIndex = matcher.end();
-                }
-            }
-        }
-
-        Pattern finalRegex = Pattern.compile(
-                "-?\\d+\\.?\\d*([+\\-/*^%]-?\\d+\\.?\\d*)+"
-        );
-        /*matcher = finalRegex.matcher(str);
+    static String parseWithRegex(String str, Pattern regex) {
+        validMatchesAmount = 0;
+        int searchStartIndex = 0;
+        Matcher matcher = regex.matcher(str);
 
         while (matcher.find(searchStartIndex)) {
             double result = Double.MAX_VALUE;
@@ -61,17 +29,37 @@ public class Parser {
                 CharSequence curMatch = matcher.group();
                 str = str.replace(matcher.group(), strResult);
                 searchStartIndex += strResult.length();
-                validMatchesCount++;
+                validMatchesAmount++;
             }
             else {
                 searchStartIndex = matcher.end();
             }
-        }*/
+        }
+
+        return str;
+    }
+    static String parseExpressions(String str) {
+        Pattern mainRegex = Pattern.compile(
+                "(cos|sin|tan|log|log2|abs)?\\(-?\\d+\\.?\\d*([+\\-/*^%]-?\\d+\\.?\\d*)*\\)"
+        );
+
+        validMatchesAmount = 1;
+        while (validMatchesAmount != 0) {
+            str = parseWithRegex(str, mainRegex);
+        }
+
+        Pattern finalRegex = Pattern.compile(
+                "-?\\d+\\.?\\d*([+\\-/*^%]-?\\d+\\.?\\d*)+"
+        );
+        str = parseWithRegex(str, finalRegex);
 
         return str;
     }
 
     public static void main(String[] args) {
         System.out.println(parseExpressions("jjj(4+sin-(3)*(3))fff5-3hh4tt"));
+        Expression exp = new ExpressionBuilder("abc3+4").build();
+        String str = String.valueOf(exp.evaluate());
+        System.out.println(str);
     }
 }
