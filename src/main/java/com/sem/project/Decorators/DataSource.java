c05ea8fe4e074f2a0562443deb74d797d133bd3a
Sashmin <tolochko.minsk@gmail.com>
2023-01-17 20:03:30 +0300

    extra commit

---
+++ src/main/java/com/sem/project/Decorators/DataSource.java
@@ -0,0 +1,15 @@
+package com.sem.project.Decorators;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public interface DataSource {
+    Path writeData() throws IOException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException;
+
+    String readData();
+}
---
+++ src/main/java/com/sem/project/Decorators/DataSourceDecorator.java
@@ -0,0 +1,30 @@
+package com.sem.project.Decorators;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class DataSourceDecorator implements DataSource {
+    static int operationNumber = 1;
+    private DataSource wrappee;
+
+    DataSourceDecorator(DataSource source) {
+        this.wrappee = source;
+    }
+
+    @Override
+    public Path writeData() throws IOException, IllegalBlockSizeException, NoSuchPaddingException,
+            NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
+        return wrappee.writeData();
+    }
+
+    @Override
+    public String readData() {
+        return wrappee.readData();
+    }
+}
